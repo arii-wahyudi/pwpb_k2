a99b72ca1d35353be2529b02f704a1105d99a4ee
arii-wahyudi <wahyudiari264@gmail.com>
2021-02-27 18:03:05 +0700

    tugas pwpb

---
+++ detail-berita.php
@@ -0,0 +1,375 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>ZeNews</title>
+  <script src="https://kit.fontawesome.com/21bd856cda.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-none">
+    <div class="container">
+      <a class="navbar-brand" href="#">Ze<span>News</span></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <form class="form-inline ml-auto my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-info my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+
+
+  <div class="container">
+    <nav>
+      <div class="menu px-5">
+        <a href="#">News</a>
+        <a href="#">Internasional</a>
+        <a href="#">Teknologi</a>
+        <a href="#">Kesehatan</a>
+        <a href="#">Otomotif</a>
+      </div>
+    </nav>
+
+    <div class="row mt-5">
+
+      <div class="col-md-8 pb-4">
+
+
+        <div class="detail-berita">
+
+          <h3 class="px-3 font-weight-bolder">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+          <img src="assets/img/berita1.jpg" alt="">
+          <p class="text-center mt-2 font-italic">Gambar : Donal Trump</p>
+          <div class="deskripsi-berita px-3 mb-5">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur in fugit sunt distinctio voluptates aut minima sed nemo corporis necessitatibus odio debitis eos nobis optio ducimus illo, quae accusantium modi</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, cum? Natus odio voluptates cum atque animi accusamus saepe, odit fugit, quia ratione facere. Numquam blanditiis aliquid, esse veritatis dolorum dolore.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum ipsam veritatis necessitatibus, consectetur, libero similique tenetur natus culpa laudantium vel alias repellat voluptates dolore neque at quisquam quasi minus possimus.</p>
+          </div>
+        </div>
+
+        <hr>
+
+        <div class="more mt-5">
+          <h3>Baca Juga</h3>
+          <hr>
+
+          <div class="row">
+
+            <div class="col-sm-4">
+              <div class="card mt-3" style="width: 100%;">
+                <img src="assets/img/berita1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="card mt-3" style="width: 100%;">
+                <img src="assets/img/berita1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="card mt-3" style="width: 100%;">
+                <img src="assets/img/berita1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="card mt-3" style="width: 100%;">
+                <img src="assets/img/berita1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="card mt-3" style="width: 100%;">
+                <img src="assets/img/berita1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="card mt-3" style="width: 100%;">
+                <img src="assets/img/berita1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <hr>
+        <!-- berita terbaru 6-10 -->
+        <div class="berita mt-5">
+          <div class="card mb-3" style="max-width: 100%;">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <img src="assets/img/berita1.jpg" class="card-img" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="card mb-3" style="max-width: 100%;">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <img src="assets/img/berita1.jpg" class="card-img" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="card mb-3" style="max-width: 100%;">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <img src="assets/img/berita1.jpg" class="card-img" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="card mb-3" style="max-width: 100%;">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <img src="assets/img/berita1.jpg" class="card-img" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="card mb-3" style="max-width: 100%;">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <img src="assets/img/berita1.jpg" class="card-img" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="card mb-3" style="max-width: 100%;">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <img src="assets/img/berita1.jpg" class="card-img" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <a href="#" class="btn btn-info float-right">
+          Lainnya
+        </a>
+      </div>
+
+
+
+      <!-- tag populer -->
+      <div class="col-md-4">
+        <div class="head-tag">
+          <hr>
+          <h5>TAG POPULER</h5>
+          <hr class="underline">
+          <div class="tag">
+            <P># <span>DAYANA</span></P>
+            <P># <span>DAYANA</span></P>
+            <P># <span>DAYANA</span></P>
+            <P># <span>DAYANA</span></P>
+          </div>
+        </div>
+
+        <!-- berita populer -->
+        <div class="populer mt-5">
+          <hr>
+          <h5>BERITA POPULER</h5>
+          <hr class="mb-3">
+          <div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+              <div class="col-md-5">
+                <img src="assets/img/berita1.jpg" class="card-img" style="min-width: 100%; min-height: 100%;" alt="...">
+              </div>
+              <div class="col-md-7">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+              <div class="col-md-5">
+                <img src="assets/img/berita1.jpg" class="card-img" alt="...">
+              </div>
+              <div class="col-md-7">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+              <div class="col-md-5">
+                <img src="assets/img/berita1.jpg" class="card-img" alt="...">
+              </div>
+              <div class="col-md-7">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+              <div class="col-md-5">
+                <img src="assets/img/berita1.jpg" class="card-img" alt="...">
+              </div>
+              <div class="col-md-7">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <br><br>
+        <div class="news">
+          <hr>
+          <h5>NEWS</h5>
+          <hr mb-3>
+
+          <div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+              <div class="col-md-5">
+                <img src="assets/img/berita1.jpg" class="card-img" style="min-width: 100%; min-height: 100%;" alt="...">
+              </div>
+              <div class="col-md-7">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+              <div class="col-md-5">
+                <img src="assets/img/berita1.jpg" class="card-img" style="min-width: 100%; min-height: 100%;" alt="...">
+              </div>
+              <div class="col-md-7">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+              <div class="col-md-5">
+                <img src="assets/img/berita1.jpg" class="card-img" style="min-width: 100%; min-height: 100%;" alt="...">
+              </div>
+              <div class="col-md-7">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+
+
+      </div>
+
+    </div>
+
+  </div>
+
+
+
+
+
+
+  <!-- footer -->
+
+  <div class="footer bg-dark text-white">
+    <div class="container">
+      <div class="row px-5 pt-4 pb-2">
+        <div class="col-md-6">
+          ZeNews
+        </div>
+        <div class="col-md-6">
+          <h4>Kategori</h4>
+          <div class="row">
+            <div class="col-sm-6">
+              <p>News</p>
+              <p>News</p>
+              <p>News</p>
+              <p>News</p>
+              <p>News</p>
+            </div>
+            <div class="col-sm-6">
+              <p>Entertainment</p>
+              <p>Entertainment</p>
+              <p>Entertainment</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <script src="assets/js/jquery.js"></script>
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
